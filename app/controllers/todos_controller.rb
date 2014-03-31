b15debc74957cf17fc42acf607611558edb1b7ec
thenickcox <nick@nickcox.me>
2014-03-30 20:43:09 -0700

    Refactor todo controller to serialize JSON properly (fixes #2)

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,61 +1,11 @@
 class TodosController < ApplicationController
-  before_action :set_todo, only: [:show, :edit, :update, :destroy]
-
-  def index
-    @todos = Todo.all
-    render :json => @todos
-  end
-
-  def show
-    render :json => @todo
-  end
-
-  def create
-    @todo = Todo.new(todo_params)
-
-    respond_to do |format|
-      if @todo.save
-        format.html { redirect_to @todo, notice: 'todo was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @todo }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @todo.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /todos/1
-  # PATCH/PUT /todos/1.json
-  def update
-    respond_to do |format|
-      if @todo.update(todo_params)
-        format.html { redirect_to @todo, notice: 'Todo was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @todo.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /todos/1
-  # DELETE /todos/1.json
-  def destroy
-    @todo.destroy
-    respond_to do |format|
-      format.html { redirect_to todos_url }
-      format.json { head :no_content }
-    end
-  end
+  respond_to :json
+  inherit_resources
 
   private
-  # Use callbacks to share common setup or constraints between actions.
-  def set_todo
-    @todo = Todo.find(params[:id])
-  end
 
   # Never trust parameters from the scary internet, only allow the white list through.
-  def todo_params
-    params.require(:todo).permit(:title, :is_completed)
+  def permitted_params
+    { todo: params.fetch(:todo, {}).permit(:title, :is_completed) }
   end
 end
