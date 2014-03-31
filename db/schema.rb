b3ad9b7731b5c9346b5a6c677ce851f1cc2b930e
Michael Ribbons <mribbons@gmail.com>
2014-04-01 07:12:38 +1100

    gem 'active_model_serializers' no longer required, now using inherited_resources
    Fix schema.rb version after unnecessary migration removal by github.com/thenickcox

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131207085127) do
+ActiveRecord::Schema.define(version: 20131207002247) do
 
   create_table "todos", force: true do |t|
     t.string   "title"
