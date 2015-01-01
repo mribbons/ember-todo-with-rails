e82dcf65d1491f63b8995b4e74476ce5cd9f5103
Eric Guo <eric.guo@sandisk.com>
2015-01-01 15:54:34 +0800

    Remove extra code

--- app/assets/javascripts/models/todo.js
+++ app/assets/javascripts/models/todo.js
@@ -2,26 +2,3 @@ Todos.Todo = DS.Model.extend({
 	title: DS.attr('string'),
 	is_completed: DS.attr('boolean')
 });
-
-/*Todos.Todo = Ember.Resource.extend({
-    resourceUrl: '/todos',
-    resourceName: 'todo'
-});*/
-
-Todos.Todo.FIXTURES = [
-{
-	id: 1,
-	title: 'Learn ember.js',
-	is_completed: true
-},
-{
-	id: 2,
-	title: '...',
-	is_completed: false
-},
-{
-	id: 3,
-	title: 'Profit!',
-	is_completed: false
-}
-];
